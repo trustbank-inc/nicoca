3252dea0856c08db8fb951c9755f17c0ef4aac27
youhei-ushio <youhei@sea-salt.jp>
2021-08-22 07:57:07 +0900

    許容するビルダーの型を追加

--- src/Components/Infrastructure/Persistence/HasSearchPhrase.php
+++ src/Components/Infrastructure/Persistence/HasSearchPhrase.php
@@ -3,7 +3,8 @@ declare(strict_types=1);
 
 namespace Seasalt\Nicoca\Components\Infrastructure\Persistence;
 
-use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\Builder as EloquentBuilder;
+use Illuminate\Database\Query\Builder as QueryBuilder;
 use Seasalt\Nicoca\Components\UseCase\SearchPhrase;
 
 trait HasSearchPhrase
@@ -11,11 +12,11 @@ trait HasSearchPhrase
     /**
      * 検索フレーズに応じたwhere句を組み立てる
      *
-     * @param Builder $builder クエリビルダ
+     * @param EloquentBuilder|QueryBuilder $builder クエリビルダ
      * @param SearchPhrase $searchPhrase 検索フレーズ
      * @param array $fields 検索対象とするフィールド名
      */
-    private function buildSearchPhraseWhereClauses(Builder $builder, SearchPhrase $searchPhrase, array $fields): void
+    private function buildSearchPhraseWhereClauses(EloquentBuilder|QueryBuilder $builder, SearchPhrase $searchPhrase, array $fields): void
     {
         if (!$searchPhrase->hasValue()) {
             return;
@@ -33,7 +34,7 @@ trait HasSearchPhrase
             $value = $searchPhrase->getValue();
             $operator = '=';
         }
-        $builder->where(function (Builder $builder) use ($fields, $value, $operator) {
+        $builder->where(function (EloquentBuilder|QueryBuilder $builder) use ($fields, $value, $operator) {
             foreach ($fields as $field) {
                 $builder->orWhere($field, $operator, $value);
             }
