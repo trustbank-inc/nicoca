d2e690918bdbe33b10fc21f81bdba005a900458a
Makoto Ishihara <89056270+ishihara-makoto-trustbank@users.noreply.github.com>
2021-09-14 13:47:45 +0900

    add: ユニットテスト追加 (#8)

--- tests/UseCase/InputValidatorTest.php
+++ tests/UseCase/InputValidatorTest.php
@@ -174,4 +174,39 @@ final class InputValidatorTest extends TestCase
             ],
         ]);
     }
+
+    /**
+     * @doesNotPerformAssertions
+     */
+    public function testCanBeValidatedOnStringArrayValue(): void
+    {
+        $validator = new InputValidator(
+            requiredFields: [
+                'quantity' => ExampleQuantity::class,
+            ],
+            optionalFields: [
+
+            ]);
+        $validator->validate([
+            'quantity' => ['2', '4', '6',],
+        ]);
+    }
+
+    /**
+     * @test
+     */
+    public function testCanBeExceptedOnStringArrayValue(): void
+    {
+        $this->expectException(InvalidInputException::class);
+        $validator = new InputValidator(
+            requiredFields: [
+                'quantity' => ExampleQuantity::class,
+            ],
+            optionalFields: [
+
+            ]);
+        $validator->validate([
+            'quantity' => ['abcde', 'efghi', 'jklmn',],
+        ]);
+    }
 }
