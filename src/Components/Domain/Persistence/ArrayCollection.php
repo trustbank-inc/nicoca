9f790c3ee0396022227e5af18e85de5cfd248e9e
youhei-ushio <youhei@sea-salt.jp>
2022-02-23 22:02:04 +0900

    配列からCollectionを作るユーティリティ

---
+++ src/Components/Domain/Persistence/ArrayCollection.php
@@ -0,0 +1,46 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Seasalt\Nicoca\Components\Domain\Persistence;
+
+use ArrayIterator;
+use Traversable;
+
+final class ArrayCollection implements Collection
+{
+    public function __construct(private array $elements)
+    {
+
+    }
+
+    public function getTotal(): int
+    {
+        return count($this->elements);
+    }
+
+    public function getLimit(): int
+    {
+        return $this->getTotal();
+    }
+
+    public function getCurrentPage(): int
+    {
+        return 1;
+    }
+
+    public function getLastPage(): int
+    {
+        return 1;
+    }
+
+    public function hasMorePages(): bool
+    {
+        return false;
+    }
+
+    public function getIterator(): Traversable
+    {
+        return new ArrayIterator($this->elements);
+    }
+}
